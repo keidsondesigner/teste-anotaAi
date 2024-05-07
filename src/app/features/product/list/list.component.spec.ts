8575cca3f2a9cd8f477aad6d5bff1da045173ada
Keidson Roby <keidsondesigner@gmail.com>
2024-05-07 02:03:48 -0400

    feat: criado teste List product

--- src/app/features/product/list/list.component.spec.ts
+++ src/app/features/product/list/list.component.spec.ts
@@ -1,17 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map, of } from 'rxjs';
+import { mockProducts } from '../../../mocks/product.mock';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ProductsService } from '../../../shared/services/products.service';
+import { normalizeString } from '../../../shared/utils/normalize-utils';
+import { Product } from '../../../models/product.model';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let httpClient: HttpClient;
+  let service: ProductsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListComponent]
-    })
-    .compileComponents();
-    
+      imports: [ListComponent, HttpClientTestingModule],
+      providers: [
+        HttpClient,
+        {
+          provide: ProductsService,
+          useValue: {
+            products$: of(mockProducts),
+            getAll: () => of(mockProducts),
+            deleteProduct: (id: number) => of({}),
+            searchProducts(searchTerm: string): Observable<Product[]> {
+              return of(mockProducts).pipe(
+                map(products => {
+                  const normalizedSearchTerm = normalizeString(searchTerm);
+                  return products.filter(product =>
+                    normalizeString(product.title).includes(normalizedSearchTerm) ||
+                    normalizeString(product.description).includes(normalizedSearchTerm)
+                  );
+                })
+              );
+            },
+          },
+        },
+      ],
+    }).compileComponents();
+
+    service = TestBed.inject(ProductsService);
+    httpClient = TestBed.inject(HttpClient);
+
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +52,22 @@ describe('ListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should search products correctly', done => {
+    const searchTerm = 'fatia';
+    component.onSearchTermChange({ target: { value: searchTerm } } as unknown as Event);
+
+    component.filteredProducts$.subscribe((products) => {
+      expect(component).toBeTruthy();
+      done();
+    });
+  });
+
+  it('should call deleteProduct correctly', () => {
+    const spy = jest.spyOn(service, 'deleteProduct');
+    component.handleOnDelete(1);
+
+    expect(spy).toHaveBeenCalledWith(1);
   });
 });
